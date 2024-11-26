f99bbf2e888c4a693ecdeca080cba8b992afd983
Manonmani Natarajan <75035658+manonmaninatarajan@users.noreply.github.com>
2024-11-26 21:41:50 +0200

    Update Binary_search.py

--- Data-structures/Search_Algorithm/Binary_search/Binary_search.py
+++ Data-structures/Search_Algorithm/Binary_search/Binary_search.py
@@ -1,1 +1,21 @@
+def binary_search(arr,search):
+    left=0
+    right=len(arr)-1
 
+    while left <=right:
+        m = (left + right) // 2
+        mid=arr[m]
+        if(mid==search):
+            print("the index of the number is:",m)
+            return
+        elif(mid<search):
+            left=m+1
+        else:
+            right=m-1
+    print("The number you entered is not in the list")
+            
+            
+            
+search=int(input("Enter the number want to search:"))        
+arr=[11,12,13,14,15,16,17,18,19]
+binary_search(arr,search)
