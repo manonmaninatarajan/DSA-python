af8576e4b222eaa582161b9272578dfc2399f1d3
Manonmani Natarajan <75035658+manonmaninatarajan@users.noreply.github.com>
2024-11-26 16:00:19 +0200

    Update Bubble_Sort.py

--- Data-structures/Array/Bubble_sort/Bubble_Sort.py
+++ Data-structures/Array/Bubble_sort/Bubble_Sort.py
@@ -1,1 +1,14 @@
+def bubblesort(arr):
+  for j in range(0,len(arr)):
+          for i in range(0,len(arr)-1):
+            if arr[i]>arr[i+1]:
+              temp=arr[i]
+              arr[i]=arr[i+1]
+              arr[i+1]=temp
+            else:
+               continue  
+  print(arr)
 
+arr=[1,2,5,3,8,3,4,6,4]
+bubblesort(arr)
+  
