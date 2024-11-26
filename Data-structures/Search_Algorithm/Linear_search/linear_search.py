0b1b6ef5875a281f13031a535d4eaaa64d488566
Manonmani Natarajan <75035658+manonmaninatarajan@users.noreply.github.com>
2024-11-26 16:13:38 +0200

    Update linear_search.py

--- Data-structures/Search_Algorithm/Linear_search/linear_search.py
+++ Data-structures/Search_Algorithm/Linear_search/linear_search.py
@@ -1,1 +1,14 @@
+print("Linear search algorithm")
+print()
 
+def linear_search(arr,value,j):
+    for i in range(j):
+        if arr[i]==value:
+            print(i)
+        else:
+            continue
+            
+ans=[1,2,23,4,5,6,7,8]
+j=len(ans)
+value=int(input("enter the value to search"))
+linear_search(ans,value,j)
