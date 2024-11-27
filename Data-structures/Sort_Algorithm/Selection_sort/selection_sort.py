4549a0f32153f6f284a0e4a54a85dfdcf0cadb94
Manonmani Natarajan <75035658+manonmaninatarajan@users.noreply.github.com>
2024-11-27 23:26:50 +0200

    Update selection_sort.py

--- Data-structures/Sort_Algorithm/Selection_sort/selection_sort.py
+++ Data-structures/Sort_Algorithm/Selection_sort/selection_sort.py
@@ -1,1 +1,17 @@
+def selection_sort(arr):
+    for i in range(0,len(arr)):        
+        min_index=i        
+        for j in range((i+1),len(arr)):            
+            if(arr[j]<arr[min_index]):
+                min_index=j               
+            else:
+                continue
+        arr[i],arr[min_index]=arr[min_index],arr[i]
+        
+    print(arr)
+        
+   
+  
+arr=[19,3,2,5,56,28,49,12,23,34,43,65,10]
 
+selection_sort(arr)
