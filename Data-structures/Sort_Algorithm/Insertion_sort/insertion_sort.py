1b2489d757b929e59a8c8ca2587b20ae8d9deec1
Manonmani Natarajan <75035658+manonmaninatarajan@users.noreply.github.com>
2024-11-28 20:49:16 +0200

    Update insertion_sort.py

--- Data-structures/Sort_Algorithm/Insertion_sort/insertion_sort.py
+++ Data-structures/Sort_Algorithm/Insertion_sort/insertion_sort.py
@@ -1,1 +1,14 @@
+import matplotlib.pyplot as plt
 
+def insertion_sort(arr):
+    i=1
+    while i<len(arr):
+        j=i
+        while j>0 and arr[j-1] > arr[j]:
+            arr[j],arr[j-1]=arr[j-1],arr[j]
+            j=j-1
+        i=i+1 
+    print(arr)
+
+arr=[1,4,3,6,34,2,8,6,67,96,45,23,12]
+insertion_sort(arr)
